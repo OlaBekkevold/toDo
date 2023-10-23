ab8e75b1043758fca15309963641191dac515731
OlaBekkevold <ola.k.bekkevold@gmail.com>
2023-10-23 11:49:27 +0200

    User can't submit an empty task
    Add an if sentence that checks the value of input and alerts the user if the value is empty
    
    #18

--- script.js
+++ script.js
@@ -14,29 +14,34 @@ function addTask() {
     descValue = descInput.value;
 
     
+    if  (titleValue == "" || descValue == "") {
+        alert("Please enter a title and description");
+        
+    } else {
+        const newTask = document.createElement("div");
+        newTask.classList.add("item");
     
-    const newTask = document.createElement("div");
-    newTask.classList.add("item");
+
+        const taskTitle = document.createElement("h2");
+        taskTitle.innerHTML = titleValue;
+        taskTitle.classList.add("itemTitle");
+        newTask.appendChild(taskTitle);
+
+        const taskDesc = document.createElement("p");
+        taskDesc.innerHTML = descValue;
+        taskDesc.classList.add("itemDesc");
+        newTask.appendChild(taskDesc);
+
+        const deleteButton = document.createElement("p");
+        deleteButton.innerHTML = "🗑️";
+        deleteButton.classList.add("deleteButton");
+        newTask.appendChild(deleteButton);
+
+
+        grid.insertBefore(newTask, grid.firstChild);
+
+        titleInput.value = "";
+        descInput.value = "";
+    }
     
-
-    const taskTitle = document.createElement("h2");
-    taskTitle.innerHTML = titleValue;
-    taskTitle.classList.add("itemTitle");
-    newTask.appendChild(taskTitle);
-
-    const taskDesc = document.createElement("p");
-    taskDesc.innerHTML = descValue;
-    taskDesc.classList.add("itemDesc");
-    newTask.appendChild(taskDesc);
-
-    const deleteButton = document.createElement("p");
-    deleteButton.innerHTML = "🗑️";
-    deleteButton.classList.add("deleteButton");
-    newTask.appendChild(deleteButton);
-
-
-    grid.insertBefore(newTask, grid.firstChild);
-
-    titleInput.value = "";
-    descInput.value = "";
 }
